aedc1afcddefd42881b078ee337797cad17981f9
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-01 21:42:28 +1100

    removed superfluous components and added CommuniHub logo

--- client/src/components/pages/Landing/Landing.js
+++ client/src/components/pages/Landing/Landing.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import './style.css';
-import LandingJumbo from '../../LandingJumbo/LandingJumbo';
-import BrandDescription from '../../BrandDescription/BrandDescription';
+import styles from './Landing.module.css';
 
 function Landing() {
     return (
         <div className="container-fluid p-0 m-0">
-            <div className="d-flex justify-content-center backgroundImg">
-                <LandingJumbo />
+            <div className={styles.backgroundImg}>
             </div>
-            <BrandDescription />
         </div>
     );
 }
