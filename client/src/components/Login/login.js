e38ac3ccafb5b753e215aa8479dd87281fb3f71f
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-02 20:48:05 +1100

    updated Login page to Mui version

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -5,85 +5,177 @@ import { loginUser } from '../../utils/userFunctions';
 import { setAuthToken } from '../../utils/setAuthToken';
 import { useHistory } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
-function Login() {
-    const history = useHistory();
-
-    const [formState, setFormState] = useState({
-        email: '',
-        password: '',
-    });
-
-    const [, appDispatch ] = useAppContext();
-
-    const onChange = (e) => {
-        setFormState({
-            ...formState,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const user = {
-            email: formState.email,
-            password: formState.password,
-        };
-        try {
-            const response = await loginUser(user);
-            // Set token to localStorage
-            const token = response.data;
-            // Set token to Auth header
-            setAuthToken(token);
-            // Decode token to get user data
-            const decodedToken = jwt_decode(token);
-            // Set current user
-            appDispatch({ type: SET_CURRENT_USER, payload: decodedToken });
-            history.push('/dashboard');
-        } catch (error) {
-            appDispatch({
-                type: GET_ERRORS,
-                payload: error,
-            });
-        }
-    };
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="mx-auto mt-5 col-md-6">
-                    <form noValidate onSubmit={handleSubmit}>
-                        <h1 className="mb-3 h3 font-weight normal">Please Sign in</h1>
-                        <div className="form-group">
-                            <label htmlFor="email">Email Address</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                name="email"
-                                placeholder="Enter Email"
-                                value={formState.email}
-                                onChange={onChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                placeholder="Enter Password"
-                                value={formState.password}
-                                onChange={onChange}
-                            />
-                        </div>
-                        <button type="submit" className="btn btn-lg btn-primary btn-block">
-                            Sign in
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        CommuniHub
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Login;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: '../../assets/images/Communihub.PNG',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const WhiteTextTypography = withStyles({
+    root: {
+      color: "#FFFFFF"
+    }
+  })(Typography);
+
+export default function Login() {
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  const [formState, setFormState] = useState({
+      email: '',
+      password: '',
+  });
+
+  const [, appDispatch ] = useAppContext();
+
+  const onChange = (e) => {
+      setFormState({
+          ...formState,
+          [e.target.name]: e.target.value,
+      });
+  };
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      const user = {
+          email: formState.email,
+          password: formState.password,
+      };
+      try {
+          const response = await loginUser(user);
+          // Set token to localStorage
+          const token = response.data;
+          // Set token to Auth header
+          setAuthToken(token);
+          // Decode token to get user data
+          const decodedToken = jwt_decode(token);
+          // Set current user
+          appDispatch({ type: SET_CURRENT_USER, payload: decodedToken });
+          history.push('/dashboard');
+      } catch (error) {
+          appDispatch({
+              type: GET_ERRORS,
+              payload: error,
+          });
+      }
+  };
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <WhiteTextTypography component="h1" variant="h5">
+            Sign in
+          </WhiteTextTypography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              onChange={onChange}
+              value={formState.email}
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={onChange}
+              value={formState.password}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
