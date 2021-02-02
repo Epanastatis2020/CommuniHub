a73bd2fac81ec9d70cfddcaa555c7e534ebd4022
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-02 20:49:30 +1100

    multiple bugfixes around user authentication, styling changes and component re-ordering

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './theme';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
