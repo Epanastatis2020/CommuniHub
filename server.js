d56746e16beeb37a139b9491dc56f105d78f282b
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-06 12:01:21 +1100

    added additional header

--- server.js
+++ server.js
@@ -54,8 +54,9 @@ require('./config/passport')(passport);
 app.use(routes);
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
