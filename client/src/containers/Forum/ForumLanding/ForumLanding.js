048c0f699711bcd2c402bf758f19c48249565d4e
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-09 15:58:09 +1100

    updated ForumLanding to include all the required components, and begin setting the required states

--- client/src/containers/Forum/ForumLanding/ForumLanding.js
+++ client/src/containers/Forum/ForumLanding/ForumLanding.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getThreads } from '../../../services/ThreadService';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
 
 import ParentForum from '../../../components/Forum/ParentForum/ParentForum';
 import FeaturedPost from '../../../components/Forum/FeaturedPost/FeaturedPost';
 import Footer from '../../../components/Footer/Footer';
 import LatestAnnouncement from '../../../components/Forum/LatestAnnouncement/LatestAnnouncement';
+import SearchBox from '../../../components/SearchBox/SearchBox';
+import Pagination from '../../../components/Pagination/Pagination';
+import TopicTable from '../../../components/Forum/TopicTable/TopicTable';
 
 const parentForum = {
   title: 'Midtown - 49-51 Denison St Wollongong',
@@ -29,7 +35,27 @@ const featuredPost = {
   postContent: 'Wow I cannot believe how quickly this forum is coming together. Anyone else?'
 };
 
+
+
 export default function ForumLanding() {
+  
+  const [topics, setTopics] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [topicsPerPage, setTopicsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      setLoading(true);
+      const res = await getThreads();
+      setTopics(res.data);
+      setLoading(false);
+    }
+
+    fetchTopics();
+  }, []);
+
+  console.log(topics);
 
   return (
     <React.Fragment>
@@ -45,6 +71,21 @@ export default function ForumLanding() {
             </Grid>
             <Grid item xs={6} s={6}>
               <FeaturedPost post={featuredPost} />
+            </Grid>
+            <Grid item xs={10} s={9}>
+              <SearchBox />
+            </Grid>
+            <Grid item xs={2} s={3}>
+              <Button variant="contained" color="primary"><SearchIcon /></Button>
+            </Grid>
+            <Grid item xs={10} s={9}>
+              <Pagination />
+            </Grid>
+            <Grid item xs={2} s={3}>
+            <Button variant="contained" color="primary">New Topic</Button>
+            </Grid>
+            <Grid item xs={12}>
+              <TopicTable />
             </Grid>
           </Grid>
         </main>
