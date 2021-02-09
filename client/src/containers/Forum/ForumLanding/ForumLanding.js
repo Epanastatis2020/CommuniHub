f3c252b684eadda15a1b52ecdd32302140aa54c2
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-09 15:02:26 +1100

    added LatestAnnouncement component and imported it in ForumLanding

--- client/src/containers/Forum/ForumLanding/ForumLanding.js
+++ client/src/containers/Forum/ForumLanding/ForumLanding.js
@@ -3,37 +3,33 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+
 import ParentForum from '../../../components/Forum/ParentForum/ParentForum';
 import FeaturedPost from '../../../components/Forum/FeaturedPost/FeaturedPost';
 import Footer from '../../../components/Footer/Footer';
+import LatestAnnouncement from '../../../components/Forum/LatestAnnouncement/LatestAnnouncement';
 
 const parentForum = {
-  title: 'Title of a longer featured blog post',
-  description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
-  imgText: 'main image description',
-  linkText: 'Continue reading…',
+  title: 'Midtown - 49-51 Denison St Wollongong',
+  image: 'https://rimh2.domainstatic.com.au/ATaM9ZUwi9t_p-g4UKcB9xslqi0=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/6e367c5b-353d-4583-b65a-6af2af82a4d8-w1440-h1200',
+  imgText: 'Midtown building',
 };
 
-const featuredPosts = [
-  {
+const latestAnnouncement = {
+  title: 'CommuniHub Admin',
+  dateStamp: 'Feb 09, 2021',
+  postLink: '#',
+  postContent: 'Just a reminder that this forum is still under construction and not all functionality is present or working'
+};
+
+const featuredPosts = {
     title: 'Featured post',
     date: 'Nov 12',
     description:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
     image: 'https://source.unsplash.com/random',
     imageText: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-];
+};
 
 export default function ForumLanding() {
 
@@ -46,9 +42,12 @@ export default function ForumLanding() {
             <Grid item xs={12}>
               <ParentForum post={parentForum} />
             </Grid>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
+            <Grid item xs={12} s={6}>
+              <LatestAnnouncement post={latestAnnouncement} />
+            </Grid>
+            <Grid item xs={12} s={6}>
+              <FeaturedPost post={featuredPosts} />
+            </Grid>
           </Grid>
         </main>
       </Container>
