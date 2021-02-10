b34de0695511b92c124a4dd48f370fab1f082f7d
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-10 16:55:17 +1100

    added framework for TopicLanding, and created Post and TopicStarter

--- client/src/containers/Forum/TopicLanding/TopicLanding.js
+++ client/src/containers/Forum/TopicLanding/TopicLanding.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getThreads } from '../../../services/ThreadService';
+import { getSpecificPosts } from '../../../services/PostService';
 
-const TopicLanding = () => {
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+
+import ParentForum from '../../../components/Forum/ParentForum/ParentForum';
+import Pagination from '../../../components/Pagination/Pagination';
+import TopicStarter from '../../../components/Forum/TopicStarter/TopicStarter';
+import Post from '../../../components/Forum/Post/Post';
+
+const TopicLanding = (props) => {
+
+    const parentForum = {
+        title: props.title,
+        image: '#',
+        imgText: props.title,
+    };
     
     return (
-        <div>Hello</div>
+    <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <main>
+            <Grid container spacing={4}>
+              <Grid item xs={12} />
+              <Grid item xs={12}>
+                <ParentForum post={parentForum} />
+              </Grid>
+              <Grid item xs={10} >
+                <Pagination />
+              </Grid>
+              <Grid item xs={2} >
+                <Button />
+              </Grid>
+              <Grid item xs={12}>
+                <TopicStarter />
+              </Grid>
+              <Grid item xs={12}>
+                {/* Function to loop through posts and render */}
+              </Grid>
+            </Grid>
+          </main>
+        </Container>
+    </React.Fragment>
     );
 };
 
