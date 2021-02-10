fb41b83adc68f80421dfd30541aed1e0dbc970a9
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-10 20:48:33 +1100

    removed cardActions since these aren't used

--- client/src/components/Forum/TopicStarter/TopicStarter.js
+++ client/src/components/Forum/TopicStarter/TopicStarter.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { blue } from '@material-ui/core/colors';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import ThumbDownAltIcon from '@material-ui/icons/ThumbDownAlt';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      height: 200,
+      height: "auto",
     },
     avatar: {
       backgroundColor: blue[500],
@@ -32,7 +26,7 @@ export default function TopicStarter(props) {
         avatar={
             <Avatar alt={topic.author} className={classes.blue} />
           }
-        title={topic.title}
+        title={topic.author}
         subheader={topic.createdAt}
       />
       <CardContent>
@@ -40,14 +34,6 @@ export default function TopicStarter(props) {
           {topic.content}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <ThumbUpAltIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ThumbDownAltIcon />
-        </IconButton>
-      </CardActions>
     </Card>
   );
 }
