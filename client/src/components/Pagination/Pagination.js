b986dd12f5ecfb72c0a716c2c15c23e62dd19c76
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-10 20:47:20 +1100

    removed styling from pagination

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
-
 export default function PaginationButtons() {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div >
       <Pagination count={10} showFirstButton showLastButton />
     </div>
   );
