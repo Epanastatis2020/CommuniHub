3bb808fa1daf0a79542ae35936a456053068d49d
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-11 11:37:23 +1100

    added frontend route for retrieving current userId and userName

---
+++ client/src/services/UserService.js
@@ -0,0 +1,28 @@
+import { axiosAuth } from './actions/axios';
+
+// get the current user
+
+export const getCurrentUserId = () => {
+    return axiosAuth
+    .post('/current-user', {})
+    .then(response => {
+        return response.data._id
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+// get a user's name
+
+export const getUserName = userID => {
+    return axiosAuth
+    .get('/user/' + userID, {
+    })
+    .then(response => {
+        return response.data.name
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
