2614e152f878d19eafdba7aeb213a459ed46fd0f
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-12 17:48:55 +1100

    added upvote and downvote button handlers

--- client/src/components/Forum/Post/Post.js
+++ client/src/components/Forum/Post/Post.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import {React, useState} from 'react';
+import { updatePost } from '../../../services/PostService';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -10,6 +12,7 @@ import Typography from '@material-ui/core/Typography';
 import { blue } from '@material-ui/core/colors';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbDownAltIcon from '@material-ui/icons/ThumbDownAlt';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -22,6 +25,42 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Post(props) {
   const classes = useStyles();
+
+  const [loading, SetLoading] = useState(false)
+  const [upvotes, SetUpvotes] = useState(props.upvotes)
+  const [downvotes, SetDownvotes] = useState(props.downvotes)
+
+  const upvoteHandler = (e) => {
+    e.preventDefault();
+    SetLoading(true)
+    let currentUpvotes = upvotes;
+    SetUpvotes(currentUpvotes + 1)
+    const postData = {
+      _id: props._id,
+      content: props.content,
+      thread_id: props.thread_id,
+      upvotes: upvotes,
+      downvotes: props.downvotes,
+    }
+    updatePost(postData)
+    SetLoading(false)    
+  }
+  
+  const downvoteHandler = (e) => {
+    e.preventDefault();
+    SetLoading(true)
+    let currentDownvotes = downvotes;
+    SetDownvotes(currentDownvotes + 1)
+    const postData = {
+      _id: props._id,
+      content: props.content,
+      thread_id: props.thread_id,
+      upvotes: props.upvotes,
+      downvotes: downvotes,
+    }
+    updatePost(postData)
+    SetLoading(false)    
+  }
 
   return (
     <Card className={classes.root}>
@@ -37,16 +76,18 @@ export default function Post(props) {
           {props.content}
         </Typography>
       </CardContent>
+      {loading ? <CircularProgress /> :
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
+        <IconButton aria-label="upvote" onClick={upvoteHandler}>
           <ThumbUpAltIcon color="inherit"/>
-          <Typography variant="caption">{props.upvotes}</Typography>
+          <Typography variant="caption">{upvotes}</Typography>
         </IconButton>
-        <IconButton aria-label="share">
+        <IconButton aria-label="downvote" onClick={downvoteHandler}>
           <ThumbDownAltIcon color="error"/>
-          <Typography variant="caption" color="error">{props.downvotes}</Typography>
+          <Typography variant="caption" color="error">{downvotes}</Typography>
         </IconButton>
       </CardActions>
+      }
     </Card>
   );
 }
