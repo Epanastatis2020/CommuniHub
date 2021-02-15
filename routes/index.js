d0716f4356fedd057d796105561373c8d2f28fb9
Epanastatis2020 <65388616+Epanastatis2020@users.noreply.github.com>
2021-02-15 13:22:43 +1100

    added ESM compatible reference to __dirname and removed transversal

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 import { currentUser, privateRoute } from '../controllers/auth.js';
 import { findOrCreateUser } from '../middleware/index.js';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import apiForumRoutes from './api/forum-routes.js';
 import apiThreadRoutes from './api/thread-routes.js';
 import apiPostRoutes from './api/post-routes.js';
 import apiUserRoutes from './api/user-routes.js';
 const router = express.Router();
-const __dirname = path.resolve();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // API Routes
 router.post('/api/current-user', findOrCreateUser, currentUser);
@@ -19,7 +21,7 @@ router.use(apiUserRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 export default router;
